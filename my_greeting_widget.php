6db999b9262c13633e5c476ff260bf06cda39ed1
ValentinGor <v.gorbatenko84@gmail.com>
2023-07-25 10:51:48 +0300

    Added base code

--- my_greeting_widget.php
+++ my_greeting_widget.php
@@ -5,3 +5,28 @@ Description: Displays the current user's greeting.
 Version: 1.0
 Author: Your name
 */
+
+
+class My_Greeting_Widget extends WP_Widget {
+    // Widget constructor
+    public function __construct() {
+        parent::__construct(
+            'my_greeting_widget',
+            'My Greeting Widget',
+            array('description' => 'Displays the current user\'s greeting.')
+        );
+    }
+
+    // Method to display in the widget
+    public function widget($args, $instance) {
+        // Take away the name of the in-line coristuvach
+        $current_user = wp_get_current_user();
+        $user_name = $current_user->display_name;
+
+        // Greetings
+        echo $args['before_widget'];
+        echo $args['before_title'] . 'Hello' . $args['after_title'];
+        echo '<p>Welcome, ' . $user_name . '!</p>';
+        echo $args['after_widget'];
+    }
+}
